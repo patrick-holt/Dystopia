8c9d0d1dcf72b6846c6fde4b189665a800f378f1
Fred <GeneralIssues@users.noreply.github.com>
2015-04-20 12:54:53 +0200

    Update PlayerClass_CS.lua

--- PlayerClass_CS.lua
+++ PlayerClass_CS.lua
@@ -19,25 +19,25 @@ PLAYER.DisplayName			= "Citizen"
 
 PLAYER.WalkSpeed			    = 400		-- How fast to move when not running
 PLAYER.RunSpeed				    = 600		-- How fast to move when running
-PLAYER.CrouchedWalkSpeed	= 0.3		-- Multiply move speed by this when crouching
+PLAYER.CrouchedWalkSpeed	    = 0.3		-- Multiply move speed by this when crouching
 PLAYER.DuckSpeed			    = 0.3		-- How fast to go from not ducking, to ducking
-PLAYER.UnDuckSpeed			  = 0.3		-- How fast to go from ducking, to not ducking
+PLAYER.UnDuckSpeed			    = 0.3		-- How fast to go from ducking, to not ducking
 PLAYER.JumpPower			    = 200		-- How powerful our jump should be
-PLAYER.CanUseFlashlight		= false		-- Can we use the flashlight
+PLAYER.CanUseFlashlight		    = false		-- Can we use the flashlight
 PLAYER.MaxHealth			    = 100		-- Max health we can have
-PLAYER.StartHealth			  = 100		-- How much health we start with
+PLAYER.StartHealth			    = 100		-- How much health we start with
 PLAYER.StartArmor			    = 0			-- How much armour we start with
-PLAYER.DropWeaponOnDie		= true		-- Do we drop our weapon when we die
-PLAYER.TeammateNoCollide	= true		-- Do we collide with teammates or run straight through them
-PLAYER.AvoidPlayers			  = true		-- Automatically swerves around other players
+PLAYER.DropWeaponOnDie  		= true		-- Do we drop our weapon when we die
+PLAYER.TeammateNoCollide	    = true		-- Do we collide with teammates or run straight through them
+PLAYER.AvoidPlayers			    = true		-- Automatically swerves around other players
 PLAYER.UseVMHands			    = true		-- Uses viewmodel hands
 
 -- Custom variables for the mod
-PLAYER.Loyalty            = 0
-PLAYER.Disloyalty         = 0
-PLAYER.Stamina            = 20
-PLAYER.BodyHeat           = 37
-PLAYER.Hunger             = 0
+PLAYER.Loyalty                  = 0
+PLAYER.Disloyalty               = 0
+PLAYER.Stamina                  = 20
+PLAYER.BodyHeat                 = 37
+PLAYER.Hunger                   = 0
 
 function PLAYER:Loadout()
 
