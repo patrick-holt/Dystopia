7cbf37a36c174f4e2d40485cfa0538c271576661
Fred <felmqu14@student.aau.dk>
2015-04-22 09:14:13 +0200

    Corrected variable names and made file ready for testing

--- PlayerClass_CS.lua
+++ PlayerClass_CS.lua
@@ -7,6 +7,8 @@ local PLAYER = {}
 
 --
 -- See gamemodes/base/player_class/player_default.lua for all overridable variables
+--
+
 -- Standard variables
 PLAYER.DisplayName			= "Citizen"
 
@@ -16,31 +18,30 @@ PLAYER.CrouchedWalkSpeed	    = 0.3		-- Multiply move speed by this when crouchin
 PLAYER.DuckSpeed			    = 0.3		-- How fast to go from not ducking, to ducking
 PLAYER.UnDuckSpeed			    = 0.3		-- How fast to go from ducking, to not ducking
 PLAYER.JumpPower			    = 200		-- How powerful our jump should be
-PLAYER.CanUseFlashlight		    = false		-- Can we use the flashlight
-PLAYER.MaxHealth			    = 100		-- Max health we can have
+PLAYER.CanUseFlashlight		    = true		-- Can we use the flashlight
+PLAYER.MaxHealth			    = 500		-- Max health we can have
 PLAYER.StartHealth			    = 100		-- How much health we start with
 PLAYER.StartArmor			    = 0			-- How much armour we start with
 PLAYER.DropWeaponOnDie  		= true		-- Do we drop our weapon when we die
-PLAYER.TeammateNoCollide	    = true		-- Do we collide with teammates or run straight through them
+PLAYER.TeammateNoCollide	    = false		-- Do we collide with teammates or run straight through them
 PLAYER.AvoidPlayers			    = true		-- Automatically swerves around other players
 PLAYER.UseVMHands			    = true		-- Uses viewmodel hands
 
 -- Custom variables for the mod
 PLAYER.Loyalty                  = 0
-PLAYER.Disloyalty               = 0
+PLAYER.SubversiveActivities     = 0
 PLAYER.Stamina                  = 20
 PLAYER.BodyHeat                 = 37
 PLAYER.Hunger                   = 0
 
 function PLAYER:Loadout()
 
-    self.Player:RemoveAllWeapons()
-    self.Player:RemoveAllItems()
-    self.Player:RemoveAllAmmo()
-    self.Player:SetLoyalty(0)
-    self.Player:SetDisloyalty(0)
-    self.Player:GiveItem( "Citizen_Clothes" )
-    self.Player:GiveItem( "ID_Card" )
+   -- self.Player:RemoveAllWeapons()
+   -- self.Player:RemoveAllItems()
+   -- self.Player:RemoveAllAmmo()
+    self.Player:GiveAmmo( 256, 	"Pistol", 		true )
+    self.Player:GiveItem( "weapon_pistol" )
+   -- self.Player:GiveItem( "ID_Card" )
 
 end
 
