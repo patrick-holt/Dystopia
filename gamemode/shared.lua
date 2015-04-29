022ae3033286809c936b00d1eac556e41c974aa4
Frederik Elmquist-Clausen <GeneralIssues@users.noreply.github.com>
2015-04-29 21:30:56 +0200

    Update shared.lua

--- gamemode/shared.lua
+++ gamemode/shared.lua
@@ -17,5 +17,5 @@ team.SetUp(3, "Rebels"  , Color(255,0,0), true ) -- Rebels have the colour Red
 
 -- Executes when the server initializes and again when the client initializes
 function GM:Initialize()
-    print("welcome to this silly server og RP'ing. Remember, this is a muslim server.")
+    print("welcome to this silly server of RP'ing. Remember, this is a muslim server.")
 end
